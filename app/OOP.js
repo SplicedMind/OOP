4e51b2417162aea71027c294e5561cf37522e814
Akinbode Femi <femi661@gmail.com>
2017-01-09 21:58:36 +0100

    Comments added

--- app/OOP.js
+++ app/OOP.js
@@ -1,14 +1,17 @@
 module.exports = {
  
 Human: function(name = 'John Doe', sex = 'not stated', age = 0, nationality = 0) {
+   //Universal properties of a human
   this.sex = sex;
   this.name = name;
   this.nationality = nationality;
   this.age = age;
+  //Properties of a human that depends on some universal property values
   this.color= (nationality === 'Nigerian' || nationality === 'Kenyan') ? 'Black' : 'White';
   this.language = (nationality === 'Nigerian') ? 'Yoruba' : 'English';
   this.voice = (sex === 'male' || sex === 'female') ? 'Lovely' : 'Horrible';
   this.apperance = (sex === 'female') ? 'Pretty' : 'Handsome';
+  //General ability of a human
   this.speak = function() {
    if (this.language === 'Yoruba')
    {
